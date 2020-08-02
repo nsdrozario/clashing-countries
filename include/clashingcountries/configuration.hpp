1640a28849717c3f16b16bfc83026723ce261dab
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-08-02 00:21:38 -0700

    implemented a basic version of init_lua_config in extend_script.cpp

--- include/clashingcountries/configuration.hpp
+++ include/clashingcountries/configuration.hpp
@@ -1,13 +1,15 @@
 #ifndef CLC_CONFIGURATION_HPP
 #define CLC_CONFIGURATION_HPP
 
+#define CLC_CONFIG_FILE_PATH "config.lua"
+
 #include <map>
 #include <vector>
 #include <string>
 
 std::vector<std::string> video_settings_keys {
     "screen_width",
-    "screen_height",
+    "screen_height"
 };
 
 std::map<std::string, int> video_settings;
--- src/extend_script.cpp
+++ src/extend_script.cpp
@@ -5,9 +5,10 @@
 lua_State **init_lua_config() {
 
     lua_State *l = luaL_newstate();
-    
+    luaL_loadfile(l, CLC_CONFIG_FILE_PATH);
     for (std::string s : video_settings_keys) {
-        
+        lua_getglobal(l, s.c_str());
+        video_settings.insert(make_pair(s, (int)lua_tonumber(l, -1)));
     }
 
     return &l;
