7bdb5e4fafdd70ac67ba76f8c4a61636e5122e6a
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-08-06 01:24:48 -0700

    started implementing command line argument processor

--- include/clashingcountries/cmdlineproc.hpp
+++ include/clashingcountries/cmdlineproc.hpp
@@ -1,12 +1,35 @@
 #ifndef CLC_CMDLINEPROC_HPP
 #define CLC_CMDLINEPROC_HPP
 
+#include <map>
+#include <vector>
+#include <string>
+
 namespace clc_cmdlineproc {
+
+    static class cmd_args {
+        public:
+            std::map<std::string, std::string> options;
+            std::vector<std::string> args;
+    };
 
     // functions to process command line arguments or something
     void init_args(int argc, char *argv[]) {
 
+        for (int i = 1; i < argc; i++) {
         
+            char *current_arg;
+            char *next_arg;
+
+            current_arg = argv[i];
+            if (current_arg[0] == '-') {
+                
+                // check if option requires an argument
+                // if not just put it in the args vector
+
+            }
+
+        }
 
     }
 
