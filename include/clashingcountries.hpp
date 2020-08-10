31186684b34a60e0fb486d51206ddbfc65dffd96
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-08-09 21:11:14 -0700

    started implementing game logic

--- include/clashingcountries.hpp
+++ include/clashingcountries.hpp
@@ -1,10 +1,44 @@
 #ifndef CLASHINGCOUNTRIES_HPP
 #define CLASHINGCOUNTRIES_HPP
 
-#include <iostream>
+#include <vector>
 
 namespace clashing_countries {
     
+    class Game {
+
+        public:
+            static std::vector<Player> players; 
+            
+    };
+
+    class Entity {
+
+        public:
+            int HP;
+
+            virtual void Attack()=0;
+
+    };
+
+    class Player : public Entity {
+
+        public:
+            int Mana;
+            int Defense;
+
+
+
+
+    };
+
+    class Powerup : public Entity {
+
+        public:
+            
+
+    };
+
 }
 
 #endif
