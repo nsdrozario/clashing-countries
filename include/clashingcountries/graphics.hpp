6837e8dbfd9f183588ea1a6590c684280f45df0a
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-08-16 16:11:12 -0700

    started rewrite of graphics.hpp

--- include/clashingcountries/graphics.hpp
+++ include/clashingcountries/graphics.hpp
@@ -5,6 +5,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <string>
+#include <functional>
 extern sf::Font text_font;
 
 bool init_font();
@@ -13,63 +14,30 @@ sf::Text gen_label(std::string text, int size);
 
 void draw_to_screen();
 
-namespace clc_ui {
+namespace clc_ui { // forgive my inconsistent naming conventions
 
-    class compound_ui_element { // class to define GUI elements that have multiple SFML elements
+    class BaseGui : public sf::Transformable, sf::Drawable {
+
+    };
+
+    class Button : BaseGui {            
 
         public:
+            std::function<bool> call; // return success or not
+
+    };
+
+    class InputBox : BaseGui {
+
+    };
     
-            void setXPosAbsolute(float x);
-            void setXPosCenter(float x);  
-            void setYPosAbsolute(float y);
-            void setYPosCenter(float y);
-            void setXSize(float x);
-            void setYSize(float y);
-            void setBackgroundColor(sf::Color c);
-            void setOutlineColor(sf::Color c);
-            void setOutlineThickness(float x);
-
-            virtual void render(sf::RenderWindow& w) = 0; // this function needs to be overloaded for each gui element
-
-        protected:
-
-            float xpos = 0.f;
-            float ypos = 0.f;
-            float x_size = 1.f;
-            float y_size = 1.f;
-            float outline_thickness = 5.0f;
-            sf::Color bg_color;
-            sf::Color outline_color;
-
-    };
-
-    class box_label : public compound_ui_element {
+    class Label : BaseGui {
 
         public:
-
-            virtual void render(sf::RenderWindow& w);
-            void setTextColor(sf::Color c);
-            void setTextSize(int x);
-            void setLabelText(std::string text);
-
-        protected:
-            
-            int text_size;
-            sf::Color text_color;
-
-            sf::RectangleShape body;
-            sf::Text label;
-
+            bool BodyVisible;
 
     };
 
-    class button : public box_label {
-
-    };
-
-    class input_field : public compound_ui_element {
-
-    };
 
 }
 
