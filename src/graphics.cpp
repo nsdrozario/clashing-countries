2d9abe130d9cdcd3a15a5329e1e04da9c28fea10
Nathaniel D'Rozario <48631513+nsdrozario@users.noreply.github.com>
2020-08-18 21:16:00 -0700

    Update graphics.cpp

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -7,11 +7,3 @@ bool init_font() {
     return text_font.loadFromFile(FONT_PATH);
 
 }
-
-sf::Text gen_label(std::string text, int size) {
-
-    sf::Text t (text, text_font, size);
-
-    return t;
-
-}
