7b26e06aa265c70a98ddf1725892c59eeec84e4a
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-09-26 14:20:39 -0700

    Started implementation of Container class

--- include/clashingcountries/graphics/Container.hpp
+++ include/clashingcountries/graphics/Container.hpp
@@ -3,13 +3,25 @@
 
 #include <SFML/Graphics.hpp>
 #include "BaseGui.hpp"
+#include <set>
 
 namespace clashing_countries {
     namespace graphics {
+
+        struct ZIndexSort {
+            bool operator() (const BaseGui *g1, const BaseGui *g2);
+        };
+
         class Container : public BaseGui {
             public:
-                std::priority_queue<BaseGui> elements;
+                std::set<BaseGui *, ZIndexSort> contents;
+
+                void addElement(BaseGui *g);
+                void removeElement(BaseGui *g);
+                
+
             private:
+                sf::RectangleShape containerBody;
                 virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
         };
     }
