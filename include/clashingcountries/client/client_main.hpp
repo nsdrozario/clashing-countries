9e301664ef4689f134e6448224017ccdb0e79d59
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-09-25 19:57:10 -0700

    successful test client build

---
+++ include/clashingcountries/client/client_main.hpp
@@ -0,0 +1,7 @@
+#ifndef CLASHING_COUNTRIES_CLIENT_MAIN_HPP
+#define CLASHING_COUNTRIES_CLIENT_MAIN_HPP
+
+#include "signal_handlers.hpp"
+#include "threads.hpp"
+
+#endif
---
+++ include/clashingcountries/client/threads.hpp
@@ -0,0 +1,10 @@
+#ifndef CLASHING_COUNTRIES_CLIENT_THREADS_HPP
+#define CLASHING_COUNTRIES_CLIENT_THREADS_HPP
+
+#include <thread>
+#include <mutex>
+
+extern std::thread game_thread;
+extern std::thread file_thread;
+
+#endif
