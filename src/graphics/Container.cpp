ea5a41071c04492b56e1dc4684764cf66bebd349
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-09-30 23:52:20 -0700

    added package target to makefile for easy distribution + fixed build errors

--- src/graphics/Container.cpp
+++ src/graphics/Container.cpp
@@ -1,8 +1,18 @@
 #include <clashingcountries/graphics.hpp>
 using namespace clashing_countries::graphics;
 
+bool ZIndexSort::operator() (const BaseGui *g1, const BaseGui *g2) {
+    return g1->zIndex > g2->zIndex;
+}
+
+
+
 void Container::draw(sf::RenderTarget &t, sf::RenderStates s) const {
 
-        
+    t.draw(containerBody);
+
+    for (BaseGui *g : contents) {
+        t.draw(*g);
+    }       
 
 }
